eeb890a925658bb5b5cd737fd3bed8dcb37d0820
Sai Praneeth Krishna Kotamraju <saipraneethkrishnakotamraju@Sais-MacBook-Pro.local>
2021-05-04 13:25:12 -0400

    Inital Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.lightBlueAccent,
+          appBar: AppBar(
+            title: Text(
+              "Ask me Anything",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+            backgroundColor: Colors.indigo,
+          ),
+          body: Ball(),
+        ),
+
       ),
     );
+
+class Ball extends StatefulWidget {
+
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int change =1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+     // ignore: deprecated_member_use
+     child: Center(child: GestureDetector(
+         onTap: (){
+           setState(() {
+             random();
+           });
+
+         },
+       child: FlatButton(child: Image.asset('images/ball$change.png')),
+     )),
+
+
+    );
+  }
+  void random(){
+
+    change = Random().nextInt(5)+1;
+  }
+
+}
